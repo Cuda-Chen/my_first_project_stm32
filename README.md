607c5dc0a84e10447763b0e301a81685ecf4594d
陳履軒 <cudachen@chenlvxuande-iMac.local>
2017-10-16 22:08:17 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # my_first_project_stm32
+
+This project has been tested and can run successfully in the following platform:
+OS: Mac OS X 10.12
+compiler: arm-GCC with sw4stm32 with Ecpilpse Oxygen
+
+Description:
+A LED-blinky hello world project on STM32F446-Nucleo64
---
+++ src/main.c
@@ -0,0 +1,40 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f4xx.h"
+
+void delay(int counter)
+{
+	int i;
+	for(i = 0; i < counter * 10000; i++) {}
+}
+
+int main(void)
+{
+	GPIO_InitTypeDef gpio;
+
+	     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+	    GPIO_StructInit(&gpio);
+	    gpio.GPIO_Pin = GPIO_Pin_5;
+	    gpio.GPIO_Mode = GPIO_Mode_OUT;
+	    GPIO_Init(GPIOA, &gpio);
+
+	    GPIO_SetBits(GPIOA, GPIO_Pin_5);
+
+	for(;;)
+		{
+		GPIO_SetBits(GPIOA, GPIO_Pin_5); // LED ON
+		        delay(1000);
+		        GPIO_ResetBits(GPIOA, GPIO_Pin_5); // LED OFF
+		        delay(1000);
+		}
+}
